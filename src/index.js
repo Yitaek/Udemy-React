dc3178c610fcb88c224e6d27452a70fa7ef4b4e1
Yitaek Hwang <yitaek@gmail.com>
2016-11-18 00:45:53 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// Create a new component
+// This componet should produce some HTML
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const App = () => {
+	return <div>Hi!</div>;
+}
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// => now replaces function in ES6
+
+// Tell React to take the component's generated HTML and put it on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
+
+// App is a class
+// To instantiate it, wrap it in JSX tag like <App />
+
+// ReactDOM wants some component and target container
+
+// Make one component per file
